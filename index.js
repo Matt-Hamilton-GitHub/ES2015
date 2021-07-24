e1061d6efc72fb6f1bb0ea5ba9813e06ec7e88a0
Matt <i.w.b.s.bilm@gmail.com>
2021-07-23 17:15:47 -0700

    class method

--- index.js
+++ index.js
@@ -242,4 +242,48 @@ function displayStudentInfo(obj){
   return `Your full name is ${first} ${last}`
 }
 
-console.log(displayStudentInfo({first: 'Elie', last:'Schoppik'}))
+// console.log(displayStudentInfo({first: 'Elie', last:'Schoppik'}))
+
+function printFullName({first,last}){
+  return `Your full name is ${first} ${last}`
+}
+
+function createStudent({likesJavaScript = true, likesES2015 = true} = {}){
+  var start = 'The student';
+  if(likesJavaScript && likesES2015){
+    start += ' likes JavaScript and ES2015'
+  } else if(likesJavaScript){
+    start += ' likes JavaScript!'
+  } else if(likesES2015){
+    start += ' likes ES2015!'
+  } else {
+    start += ' does not like much...'
+  }
+  return start;
+}
+
+function reverseArray(arr){
+  for(var i = 0; i < arr.length/2; i++){
+    [arr[i], arr[arr.length - 1 - i]] = [arr[arr.length - 1 - i], arr[i]]
+  }
+  return arr;
+}
+
+
+
+// -------------------------- Part II ----------------------
+
+
+      // CLASS
+
+      class Student{
+        constructor(firstName, lastName){
+          this.firstName = firstName;
+          this.lastName = lastName;
+        }
+      }
+
+      var matt = new Student('Matt', 'Hamilton')
+
+      console.log(matt);
+
