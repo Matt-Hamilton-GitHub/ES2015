644c5641f9dad41c31045d76f48c5710fd6c6647
Matt <i.w.b.s.bilm@gmail.com>
2021-09-05 19:29:02 -0700

    object - assign

--- generator.js
+++ generator.js
@@ -1,6 +1,10 @@
 
 var original = {name: 'Matt'};
-var newObj = Object.assign({}, original);
+// assign value to a new variable nto changing the original value
+var newObj = Object.assign({}, original); 
 newObj.name = 'Kevin'
 console.log(`${newObj.name} --- ${original.name}`);
 
+//coverting an array-like-object into an array
+var divs = document.getElementsByTagName('div');
+var converted = Array.from(divs)
